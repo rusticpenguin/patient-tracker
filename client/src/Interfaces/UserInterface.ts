a01007b717fdbf32859a18dc3fc50515845ca5cb
rusticpenguin <georgechios.music@gmail.com>
2021-08-18 23:15:20 -0600

    feat(interfaces): adds interfaces to aid project

---
+++ client/src/Interfaces/UserInterface.ts
@@ -0,0 +1,31 @@
+interface CoreUser {
+  username: string,
+  dateOfBirth: number,
+  phoneNumber: string,
+  email: string,
+  address: string,
+  photoUrl: string,
+}
+
+export interface UserData extends CoreUser {
+  roles: string[],
+  appointments: Appointment[],
+  lastLogin: number,
+  accountCreated: number
+}
+
+export interface UserRegistationData extends CoreUser {
+  appointment: Appointment
+}
+
+export interface Appointment {
+  provider: string,
+  dateCreate?: number,
+  appointmentTime: number,
+  isCanceled?: boolean
+}
+
+export interface CacheData {
+  currentUser: UserData,
+  viewableUsers: UserData[]
+}
